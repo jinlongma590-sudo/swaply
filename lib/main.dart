0b0a5a67941ebc973b5dd30e9e72f09a01af7894
JinLong Ma <jinlongma590@gmail.com>
2025-11-25 15:39:18 +0800

    feat: auth refresh + sliver fixes + avatar upload; add AuthGate; UI+bugfixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.green,
         body: Center(
           child: Text(
-            'MAIN TEST OK',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+            'PURE MAIN OK',
+            style: TextStyle(fontSize: 40, color: Colors.white),
           ),
         ),
       ),
